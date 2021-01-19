fd2e7bb2256a9e23ca12e1a8c8f727d1b0c23cb7
Armaan <armaanmaani@gmail.com>
2021-01-19 01:30:17 -0500

    Form and toggles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-import { useState } from 'react'
+import AddTask from './components/AddTask'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([{
     id: 1,
     text: 'Get a Job',
@@ -10,15 +12,30 @@ function App() {
     reminder: true
   }])
 
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask])
+   }
+
   //Delete Task
   const deleteTask = (id) => {
-    setTasks(tasks.filter(task => task.id != id))
+    setTasks(tasks.filter(task => task.id !== id))
   }
 
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map(task => task.id === id ?
+      { ...task, reminder: !task.reminder } : task))
+    
+  }
+ 
   return (
     <div className="container">
-      <Header title= 'Task Tracker'/>
-      {tasks.length > 0  ? <Tasks tasks={tasks} onDelete={deleteTask}/> : <h3 style={{ color: 'Blue'}}>No Tasks</h3>}
+      <Header title='ਮਾਨ Task Tracker' onAdd={() => setShowAddTask(!showAddTask)} showAdd={ showAddTask }/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : <h3 style={{ color: 'Blue'}}>No Tasks</h3>}
+      {showAddTask && <AddTask onAdd={ addTask }/>}
     </div>
   );
 }
